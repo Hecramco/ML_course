55747a0d19963b9da15474508e5f262c326e1311
Hecramco <tolomancobain@gmail.com>
2022-10-07 10:38:49 +0200

    Primer controld e cambios

--- labs/ex02/template/costs.py
+++ labs/ex02/template/costs.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 
-def compute_loss(y, tx, w):
+def compute_loss(y, tx, w, loss):
     """Calculate the loss using either MSE or MAE.
 
     Args:
@@ -18,7 +18,16 @@ def compute_loss(y, tx, w):
         the value of the loss (a scalar), corresponding to the input parameters w.
     """
     # ***************************************************
-    # INSERT YOUR CODE HERE
-    # TODO: compute loss by MSE
+    e = (y-np.dot(tx,w))
+    
+    match loss: 
+        case 'mse':
+            mse = (1/2)*np.mean(e**2)
+            return mse , e
+        case 'mae':
+            mae = np.mean(np.abs(e))
+            return mae, e
+        case other:
+            print("incorrect loss type, use 'mae' or 'mse' " )
     # ***************************************************
-    raise NotImplementedError
+    #raise NotImplementedError
