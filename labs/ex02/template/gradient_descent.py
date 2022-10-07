4fe3369fb157d833639d864f4945aecbf900d166
Hecramco <tolomancobain@gmail.com>
2022-10-07 10:44:03 +0200

    Segundo control de cambios

--- labs/ex02/template/gradient_descent.py
+++ labs/ex02/template/gradient_descent.py
@@ -41,22 +41,15 @@ def gradient_descent(y, tx, initial_w, max_iters, gamma):
     ws = [initial_w]
     losses = []
     w = initial_w
+    #gradient = compute_gradient(y, tx, initial_w)
     for n_iter in range(max_iters):
-        # ***************************************************
-        # INSERT YOUR CODE HERE
-        # TODO: compute gradient and loss
-        # ***************************************************
-        raise NotImplementedError
-        # ***************************************************
-        # INSERT YOUR CODE HERE
-        # TODO: update w by gradient
-        # ***************************************************
-        raise NotImplementedError
-
+        gradient = compute_gradient(y, tx, w)
+        loss = compute_loss(y, tx, w, 'mse')
+        w = w - gamma*gradient #here we compute w(t+1) = w(t) - gamma*Grad(error)
         # store w and loss
         ws.append(w)
         losses.append(loss)
         print("GD iter. {bi}/{ti}: loss={l}, w0={w0}, w1={w1}".format(
-            bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
+              bi=n_iter, ti=max_iters - 1, l=loss, w0=w[0], w1=w[1]))
 
     return losses, ws
