c0d0490dffdca027f755e9b589d0bad52f77bc9c
Hecramco <tolomancobain@gmail.com>
2022-10-13 11:11:38 +0200

    Minor changes in my code e3

--- labs/ex03/template/plots.py
+++ labs/ex03/template/plots.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 
 def plot_fitted_curve(y, x, weights, degree, ax):
     """plot the fitted curve."""
+    fig = plt.figure()  # an empty figure with no Axes
+    fig, ax = plt.subplots()  # a figure with a single Axes
     ax.scatter(x, y, color='b', s=12, facecolors='none', edgecolors='r')
     xvals = np.arange(min(x) - 0.1, max(x) + 0.1, 0.1)
     tx = build_poly(xvals, degree)
@@ -15,6 +17,7 @@ def plot_fitted_curve(y, x, weights, degree, ax):
     ax.set_xlabel("x")
     ax.set_ylabel("y")
     ax.set_title("Polynomial degree " + str(degree))
+    fig.show()
 
 def plot_train_test(train_errors, test_errors, lambdas, degree):
     """
