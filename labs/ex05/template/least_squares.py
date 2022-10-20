0fc556c8faee68635c1075f98a76519cae6c9a90
Hecramco <tolomancobain@gmail.com>
2022-10-20 13:06:11 +0200

    Started with ex05
    Logistic reg

--- labs/ex05/template/least_squares.py
+++ labs/ex05/template/least_squares.py
@@ -6,12 +6,42 @@ Least Square
 
 import numpy as np
 
+def compute_mse(y, tx, w):
+    """compute the loss by mse.
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+        w: weights, numpy array of shape(D,), D is the number of features.
+    
+    Returns:
+        mse: scalar corresponding to the mse with factor (1 / 2 n) in front of the sum
+
+    >>> compute_mse(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]), np.array([0.03947092, 0.00319628]))
+    0.006417022764962313
+    """
+    
+    e = y - tx.dot(w)
+    mse = e.dot(e.T) / (2 * len(e))
+    return mse
 
 def least_squares(y, tx):
-    """calculate the least squares."""
+    """Calculate the least squares solution.
+       returns mse, and optimal weights.
+    
+    Args:
+        y: numpy array of shape (N,), N is the number of samples.
+        tx: numpy array of shape (N,D), D is the number of features.
+    
+    Returns:
+        w: optimal weights, numpy array of shape(D,), D is the number of features.
+        mse: scalar.
+
+    >>> least_squares(np.array([0.1,0.2]), np.array([[2.3, 3.2], [1., 0.1]]))
+    (array([ 0.21212121, -0.12121212]), 8.666684749742561e-33)
+    """
     # ***************************************************
-    # INSERT YOUR CODE HERE
-    # least squares: TODO
-    # returns mse, and optimal weights
-    # ***************************************************
-    raise NotImplementedError
+    gram = np.dot(np.transpose(tx), tx)     # This is a DXD matrix
+    w = np.ones(tx.shape[1],)              # This is a D,1 vector 
+    w = np.linalg.solve(gram, np.dot(tx.T,y)) # This must solve a DXD * DxN*Nx1,
+    mse = compute_mse(y,tx, w)
+    return w, mse
